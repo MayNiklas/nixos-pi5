d79d64a648dcc637cd86f8929001c6f803eda821
MayNiklas <info@niklas-steffen.de>
2023-11-17 09:53:51 +0100

    add config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nixos-pi5
+
+Try to get the Raspberry Pi 5 working with NixOS.
+Therefor, I'm building bootable images with different kernels and bootloaders.
+
+| Kernel version | Image link       | Status |
+| -------------- | ---------------- | ------ |
+| 6.1.21         | [Download](TODO) | TODO   |
--- flake.nix
+++ flake.nix
@@ -5,38 +5,43 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }: {
+  outputs = { self, nixpkgs, nixos-hardware, ... }:
+    let
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ ]; });
+    in
+    {
 
-    # nix build .\#nixosConfigurations.pi5.config.system.build.sdImage
-    nixosConfigurations.pi5 =
-      let
-        system = "aarch64-linux";
-        pkgs = (import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
-        });
-      in
-      nixpkgs.lib.nixosSystem {
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-          nixos-hardware.nixosModules.raspberry-pi-4
-          {
-            nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
-            nix.registry.nixpkgs.flake = nixpkgs;
-            sdImage.compressImage = false;
-            sdImage.imageBaseName = "raspi-image";
-            nixpkgs.overlays = [
-              (final: super: {
-                makeModulesClosure = x:
-                  super.makeModulesClosure (x // { allowMissing = true; });
-              })
-            ];
-            nixpkgs.hostPlatform = pkgs.lib.mkDefault "${system}";
-            system.stateVersion = "23.11";
-          }
-        ];
-      };
+      # nix build .\#pi5-image
+      packages = forAllSystems (system: {
+        pi5-image = self.nixosConfigurations.pi5.config.system.build.sdImage;
+      });
 
-  };
+      nixosConfigurations.pi5 = let system = "aarch64-linux"; pkgs = nixpkgsFor.${system}; in
+        nixpkgs.lib.nixosSystem {
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            nixos-hardware.nixosModules.raspberry-pi-4
+            {
+              # try out kernel versions here
+              # boot.kernelPackages = pkgs.linuxPackages_6_1;
+              nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+              nix.registry.nixpkgs.flake = nixpkgs;
+              sdImage.compressImage = false;
+              sdImage.imageBaseName = "pi5-image";
+              nixpkgs.overlays = [
+                (final: super: {
+                  makeModulesClosure = x:
+                    super.makeModulesClosure (x // { allowMissing = true; });
+                })
+              ];
+              nixpkgs.hostPlatform = pkgs.lib.mkDefault "${system}";
+              system.stateVersion = "23.11";
+            }
+          ];
+        };
+
+    };
 
 }
